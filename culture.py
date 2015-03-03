16414f4f517ab69376c7eb77d11975f8efafb32c
Jacob Peck <gatesphere@gmail.com>
2015-03-03 13:33:05 -0500

    Population phase works.

--- culture.py
+++ culture.py
@@ -7,6 +7,7 @@
 #@+<< imports >>
 #@+node:peckj.20140519080415.3929: ** << imports >>
 import random
+import inflect
 #@-<< imports >>
 #@+<< declarations >>
 #@+node:peckj.20140519080415.3930: ** << declarations >>
@@ -14,6 +15,8 @@ GAMETURNS = 50
 
 STARTING_POPULATIONS = 10
 STARTING_CITIES = 1
+
+p = inflect.engine()
 #@-<< declarations >>
 
 #@+others
@@ -22,7 +25,30 @@ class Player:
   def __init__(self, name, populations=STARTING_POPULATIONS, cities=STARTING_CITIES):
     self.name = name
     self.populations = populations
+    self.populations_distributions = {'agriculture': populations,
+                                      'trade': 0,
+                                      'labor': 0,
+                                      'scholars': 0,
+                                      'armies': 0,
+                                      'navies': 0}
+    self.leaders = {'ruler': 0,
+                    'general': 0,
+                    'thinker': 0,
+                    'builder': 0,
+                    'religious': 0,
+                    'diplomat': 0}            
     self.cities = cities
+    self.natural_resources = {'grain': 0,
+                              'fish': 0,
+                              'iron': 0,
+                              'horses': 0,
+                              'precious metals': 0,
+                              'fruits': 0,
+                              'livestock': 0,
+                              'wood': 0} # stub
+    self.manufactured_resources = {} # stub
+    self.advances = []
+                              
 #@+node:peckj.20140519080415.3932: ** roll
 def roll(sides=6, n=1):
   total = 0
@@ -60,10 +86,13 @@ def population_phase(player):
   # affected by number of Religious leaders (+1d6 each)
   # affected by Monotheism (+1, so 1d6 - 1 min 0)
   
-  ## STUB
-  pop_growth = max(0,roll() - 2)
+  mod = -2
+  if 'monotheism' in player.advances: mod = -1
+  pop_growth = max(0, roll() + mod)
+  for i in range(player.leaders['religious']):
+    pop_growth += roll()
   player.populations += pop_growth
-  print "You've gained %s population this turn. New total: %s" % (pop_growth, player.populations)
+  print "You've gained %s %s this turn. New total: %s" % (pop_growth, p.plural('population', pop_growth), player.populations)
 #@-others
 
 if __name__=='__main__':
